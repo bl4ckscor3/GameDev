c67f9eb039b2159ddd1d93be0254f4854a022c5c
bl4ckscor3 <donalddani9612@gmx.de>
2014-09-23 21:02:18 +0200

    without this the game wouldn't work

---
+++ GameDev/src/bl4ckscor3/game/GameDev/core/Core.java
@@ -0,0 +1,27 @@
+package bl4ckscor3.game.GameDev.core;
+
+import javax.swing.JFrame;
+
+/**
+ * bl4ckscor3 - 23.09.2014
+ */
+public class Core
+{
+	public static void main(String[] args)
+	{
+		new Core();
+	}
+	
+	public Core()
+	{
+		JFrame frame = new JFrame();
+		
+		frame.setTitle("GameDev");
+		//setting window to full size of screen
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//getting rid of the border (pseudo fullscreen now created)
+		frame.setUndecorated(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
