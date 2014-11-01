793acbe3d3c902bea51816eea6755d05f34407b7
bl4ckscor3 <donalddani9612@gmx.de>
2014-11-01 18:25:58 +0100

    simplex noise done

--- GameDev/src/bl4ckscor3/game/GameDev/world/generation/SimplexNoiseOctave.java
+++ GameDev/src/bl4ckscor3/game/GameDev/world/generation/SimplexNoiseOctave.java
@@ -1,5 +1,6 @@
 package bl4ckscor3.game.GameDev.world.generation;
 
+import java.awt.Point;
 import java.util.Random;
 
 public class SimplexNoiseOctave
@@ -22,7 +23,10 @@ public class SimplexNoiseOctave
 	private short[] p = p_supply.clone();
 	private short[] perm = new short[512];
 	private short[] permMod12 = new short[512];
-	
+	private Point[] points = {new Point(1, 1), new Point(-1, 1), new Point(1, -1), new Point(-1, -1)};
+	private final double f2 = 0.5 * (Math.sqrt(3.0) - 1.0);
+	private final double g2 = (3.0 - Math.sqrt(3.0)) / 6.0;
+		
 	public SimplexNoiseOctave(int seed)
 	{
 		Random r = new Random(seed);
@@ -45,6 +49,100 @@ public class SimplexNoiseOctave
 			perm[i] = p[i & 255];
 			permMod12[i] = (short) (perm[i] % 4);
 		}
+	}
+	
+	//2d simplex noise
+	public double noise(double xIn, double yIn)
+	{
+		double n0;
+		double n1;
+		double n2;
+		double s = (xIn + yIn) * f2;
+		int i = fastFloor(xIn + s);
+		int j = fastFloor(yIn + s);
+		double t = (i + j) * g2;
+		double X0 = i - t;
+		double Y0 = j - t;
+		double x0 = xIn - X0;
+		double y0 = yIn - Y0;
+		double x1;
+		double y1;
+		double x2;
+		double y2;
+		double t0;
+		double t1;
+		double t2;
+		int i1;
+		int j1;
+		int ii;
+		int jj;
+		int gi0;
+		int gi1;
+		int gi2;
+		
+		if(x0 > y0)
+		{
+			i1 = 1;
+			j1 = 0;
+		}
+		else
+		{
+			i1 = 0;
+			j1 = 1;
+		}
+		
+		x1 = x0 - i1 + g2;
+		y1 = y0 - j1 + g2;
+		x2 = x0 - 1.0 + 2.0 * g2;
+		y2 = y0 - 1.0 + 2.0 * g2;
+		ii = i & 255; //making sure that it's not greater than 255
+		jj = j & 255;
+		gi0 = permMod12[ii + perm[jj]];
+		gi1 = permMod12[ii + i1 + perm[jj + j1]];
+		gi2 = permMod12[ii + 1 + perm[jj + 1]];
+		t0 = 0.5 - x0 * x0 - y0 * y0;
+		t1 = 0.5 - x1 * x1 - y1 * y1;
+		t2 = 0.5 - x2 * x2 - y2 * y2;
+		
+		if(t0 < 0)
+			n0 = 0;
+		else
+		{
+			t0 *= t0;
+			n0 = t0 * t0 * dot(points[gi0], x0, y0);
+		}
+		
+		if(t1 < 0)
+			n1 = 0;
+		else
+		{
+			t1 *= t1;
+			n1 = t1 * t1 * dot(points[gi1], x1, y1);
+		}
+		
+		if(t2 < 0)
+			n2 = 0;
+		else
+		{
+			t2 *= t2;
+			n2 = t2 * t2 * dot(points[gi2], x2, y2);
+		}
+		
+		return 70 * (n0 + n1 + n2);
+	}
+	
+	//just a useful method
+	private double dot(Point p, double x, double y)
+	{
+		return p.x * x + p.y * y;
+	}
+	
+	//rounding down a double to an int
+	private int fastFloor(double x)
+	{
+		int xi = (int) x;
+		
+		return x < xi ? xi - 1 : xi;
 	}
 	
 	//8-bit system
