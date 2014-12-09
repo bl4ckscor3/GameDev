0f2fbef25b731c006d123c8d4f88c23ed4a98333
bl4ckscor3 <donalddani9612@gmx.de>
2014-12-09 14:42:16 +0100

    added support for arrow keys + slowed down movement by half

--- GameDev/src/bl4ckscor3/game/GameDev/game/Game.java
+++ GameDev/src/bl4ckscor3/game/GameDev/game/Game.java
@@ -1,6 +1,5 @@
 package bl4ckscor3.game.GameDev.game;
 
-import bl4ckscor3.game.GameDev.core.Main;
 import bl4ckscor3.game.GameDev.entity.Player;
 import bl4ckscor3.game.GameDev.listener.Key;
 import bl4ckscor3.game.GameDev.world.Map;
@@ -14,7 +13,7 @@ public class Game
 	public int ups;
 	public static int mousePosX;
 	public static int mousePosY;
-	
+
 	public Game()
 	{
 		Player player = new Player();
@@ -24,21 +23,25 @@ public class Game
 		thread = new GameThread();
 	}
 
-	public static void update()
+	public static void update(int moveTimer)
 	{
 		map.checkChunks();
 
-		//update keys
-		for(int key : Key.keysPressed)
+		//making the player able to only move every other tick
+		if(moveTimer % 2 == 0)
 		{
-			if(key == 87) //w
-				map.player.posY--;
-			else if(key == 65) //a
-				map.player.posX--;
-			else if(key == 83) //s
-				map.player.posY++;
-			else if(key == 68) //d
-				map.player.posX++;
+			//update keys
+			for(int key : Key.keysPressed)
+			{
+				if(key == 87 || key == 38) //w or up arrow
+					map.player.posY--;
+				else if(key == 65 || key == 37) //a or left arrow
+					map.player.posX--;
+				else if(key == 83 || key == 40) //s or down arrow
+					map.player.posY++;
+				else if(key == 68 || key == 39) //d or right arrow
+					map.player.posX++;
+			}
 		}
 	}
 
--- GameDev/src/bl4ckscor3/game/GameDev/game/GameThread.java
+++ GameDev/src/bl4ckscor3/game/GameDev/game/GameThread.java
@@ -28,7 +28,7 @@ public class GameThread extends Thread implements Runnable
 			
 			while(System.currentTimeMillis() > nextGameTick && loops < maxFrameSkips)
 			{
-				Game.update();
+				Game.update(ticks);
 				nextGameTick += gameSkipTicks; //makes sure to wait 16ms before updating again
 				ticks++;
 				loops++;
