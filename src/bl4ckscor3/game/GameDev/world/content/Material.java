fdd8659f0d4fc1402ce8d5c12468872bfe806a9c
bl4ckscor3 <donalddani9612@gmx.de>
2016-01-15 13:36:32 +0100

    changed material to enum and removed unneccessary stuff

--- src/bl4ckscor3/game/GameDev/world/content/Material.java
+++ src/bl4ckscor3/game/GameDev/world/content/Material.java
@@ -1,37 +1,20 @@
 package bl4ckscor3.game.GameDev.world.content;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-public class Material
+public enum Material
 {
-	//all the materials
-	public static final Material GRASS = new Material("grass");
-	public static final Material SAND = new Material("sand");
-	public static final Material WATER_NORMAL = new Material("water_normal");
-	public static final Material WATER_DEEP = new Material("water_deep");
-	//no more materials
+	GRASS("grass"),
+	SAND("sand"),
+	WATER_NORMAL("water_normal"),
+	WATER_DEEP("water_deep");
+
 	/** The ID of the Material (used to determine which material it is)*/
 	private String resourceID;
-	/** A list of all known IDs*/
-	private List<String> resourceIDs = new ArrayList<String>();
-	
-	public Material(String resourceID)
+
+	private Material(String resourceID)
 	{
 		this.resourceID = resourceID;
-		resourceIDs.add(resourceID);
 	}
 	
-	/**
-	 * Returns the resourceID at the given position of the array
-	 * @param i - The array position to get the ID from
-	 */
-	public String getResourceID(int i)
-	{
-		return resourceIDs.get(i);
-	}
-
 	/**
 	 * Returns the resourceID of the Material
 	 * @return The resource ID
@@ -39,15 +22,5 @@ public class Material
 	public String getResourceID()
 	{
 		return resourceID;
-	}
-	
-	/**
-	 * Returns a random loaded resourceID
-	 */
-	public String getRandomResourceID()
-	{
-		Random r = new Random();
-		
-		return resourceIDs.get(r.nextInt(resourceIDs.size()));
 	}
 }
